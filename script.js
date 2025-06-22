af7519aca6cea248f6d6b923fd969853cf7d35a1
saul999a1 <saul999a@outlook.com>
2025-06-22 18:42:42 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,19 +17,3 @@ function info() {
   alert("ℹ️ FixSaTec es una Mini App diseñada para ayudarte con diagnósticos y soporte técnico.");
 }
 
-.bienvenida {
-  background-color: #f9f9f9;
-  border-radius: 10px;
-  padding: 20px;
-  text-align: center;
-}
-
-.bienvenida h2 {
-  color: #333;
-}
-
-.bienvenida button {
-  margin: 10px;
-  padding: 10px 20px;
-  font-size: 16px;
-}
